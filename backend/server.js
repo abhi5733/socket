5d9b670d5c9953d741352bd4777e0dd6059acec4
abhi5733 <abhijeetkale5733@gmail.com>
2024-03-07 15:56:16 +0530

    added socket.io

--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,19 @@ const app = express()
 
 const server = createServer(app)
 
-const io = new Server(server , {
-    cors :  {
-    origin: "http://localhost:5173",
-    methods: ["GET" , "POST" ],
-    credentials : true
-} ,})
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    methods: ["GET" , "POST" ],
-    credentials : true
-}))
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true
+}));
 
 app.get("/" , (req,res)=>{
     res.send("Hello world")
