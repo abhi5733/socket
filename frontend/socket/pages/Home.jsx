d02aa487879236ede6c7159324360dd3bf82b724
abhi5733 <abhijeetkale5733@gmail.com>
2024-04-24 17:02:05 +0530

    bugs fixed

--- frontend/socket/pages/Home.jsx
+++ frontend/socket/pages/Home.jsx
@@ -640,8 +640,9 @@ const leaveGroup = ()=>{
   
 return  <Box key={el._id} ml={(el.name || el.senderName)==info.name?"50%":"0%"} justifySelf={"end"}  w={"50%"} padding={2} > <Box  w={[ "100%" , "70%" , "60%" ,"70%"]} bgColor={(el.name || el.senderName)==info.name?"green.300":"blue.200"} p={5} borderRadius={"20px"}  ><Text fontWeight={"bold"} fontSize={"20px"} > {(el.name || el.senderName)==info.name?"You":(el.name || el.senderName)}</Text> 
 
-{el.message.includes("cloudinary")?el.message.includes("pdf")? <iframe  src={el.message} width={"100%"} height="80%" frameborder="0"></iframe> :<Image src={el.message} alt="Image Deleted" />:<Text> {el.message}</Text>}</Box>  </Box>  
+{el.message.includes("cloudinary")?el.message.includes("pdf")?   <embed src={el.message} width={"100%"} height="80%" type="application/pdf" /> :<Image src={el.message} alt="Image Deleted" />:<Text> {el.message}</Text>}</Box>  </Box>  
   })}</Box>}
+  {/* <iframe  src={el.message} width={"100%"} height="80%" frameborder="0"></iframe>  */}
 
 {/* Message Box */} 
 
